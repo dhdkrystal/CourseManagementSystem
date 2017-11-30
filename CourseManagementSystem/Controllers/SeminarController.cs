6c59cfed68cc23161b4bca5c5f8c8b265d5e4e42
少岩 崔 <569736866@qq.com>
2017-11-30 16:24:32 +0800

    界面

---
+++ CourseManagementSystem/Controllers/SeminarController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseManagementSystem.Controllers
+{
+    public class SeminarController : Controller
+    {
+        // GET: Seminar
+        public ActionResult Seminar()
+        {
+            return View();
+        }
+
+        public ActionResult GradePresentationEndUI()
+        {
+            return View();
+        }
+
+        public ActionResult GradePresentationUI()
+        {
+            return View();
+        }
+
+        public ActionResult RandomGroupChooseTopicUI()
+        {
+            return View();
+        }
+
+        public ActionResult RandomGroupChooseTopicUI2()
+        {
+            return View();
+        }
+
+        public ActionResult RandomGroupLeaderUI()
+        {
+            return View();
+        }
+
+        public ActionResult RandomGroupMemeberUI()
+        {
+            return View();
+        }
+
+        public ActionResult RandomGroupNoLeaderUI()
+        {
+            return View();
+        }
+
+        public ActionResult StudentRollCallEndUI()
+        {
+            return View();
+        }
+
+        public ActionResult StudentRollCallLateUI()
+        {
+            return View();
+        }
+
+        public ActionResult StudentRollCallUI()
+        {
+            return View();
+        }
+    }
+}
