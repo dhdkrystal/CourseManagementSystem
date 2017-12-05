4bf590c0649957b95d5593e2b158165bd5ce7df2
dhdkrystal <609490912@qq.com>
2017-12-05 20:00:43 +0800

    modify

--- CourseManagementSystem/Controllers/MeController.cs
+++ CourseManagementSystem/Controllers/MeController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using System.Web;
 using CourseManagementSystem.Models;
 using CourseManagementSystem.DTOs;
+using CourseManagementSystem.ViewModels;
 
 namespace CourseManagementSystem.Controllers
 {
